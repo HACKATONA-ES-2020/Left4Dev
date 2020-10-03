9b83a68ee13a6dac2e20912bbedd8d507306a6d2
guilhermepdc <guilherme.carvalho@edu.pucrs.br>
2020-10-03 14:04:51 -0300

    Ambiente React

---
+++ frontend/src/pages/Landing/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+function Landing() {
+  return (
+    <div className="home">
+      <header className="home-header">
+        <h1>Landing Page</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Landing;
