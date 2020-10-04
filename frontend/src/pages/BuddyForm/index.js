e1787973afea6bd5d531eab2cca3247699459eb7
Matheus <matheus.tosin@edu.pucrs.br>
2020-10-04 02:19:53 -0300

    removendo imports

--- frontend/src/pages/BuddyForm/index.js
+++ frontend/src/pages/BuddyForm/index.js
@@ -6,16 +6,11 @@ import { Container, TextField} from '@material-ui/core';
 import PageHeader from '../../components/PageHeader';
 import Background from '../../components/Background';
 
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
-import Input from '@material-ui/core/Input';
-import FilledInput from '@material-ui/core/FilledInput';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
 import InputLabel from '@material-ui/core/InputLabel';
 import InputAdornment from '@material-ui/core/InputAdornment';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
