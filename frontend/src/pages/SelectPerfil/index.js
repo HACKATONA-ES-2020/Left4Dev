85d9a649c74bc5390876e5db1c8f2b45d0691953
guilhermepdc <guilherme.carvalho@edu.pucrs.br>
2020-10-04 09:11:36 -0300

    layout home

--- frontend/src/pages/SelectPerfil/index.js
+++ frontend/src/pages/SelectPerfil/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.css';
 import { Button, Typography } from '@material-ui/core';
 import Background from '../../components/Background';
-import Logo from '../../assets/img/logo.png';
+import Logo from '../../assets/img/logovec.png';
 import { Link } from 'react-router-dom';
 
 
@@ -10,35 +10,36 @@ function SelectPerfil() {
   return (
     <div className="inicial-container">
       <Background>
-          <img src={Logo} alt="Buddy" />
+        <img src={Logo} alt="Buddy" />
         <Typography variant="h1" color="primary" align="center">
-          Bem vindo ao Buddy!
+          Bem vindo
         </Typography>
         <div className="description">
           <Typography variant="h4" color="primary">
-            Aqui você pode desabafar com pessoas dispostas a ajudar e interagir, primeiro precisamos saber se você quer ser ouvido ou quer ser um buddy.
-            O buddy é com quem você será conectado para conversar, você fala, o buddy escuta!
+           Este é o espaço para você desabafar com pessoas dispostas a ajudar e interagir, mas primeiro precisamos saber se você quer ser ouvido ou quer ser um buddy.
+            <br/>O buddy é com quem você será conectado para conversar, você fala, o buddy escuta!
           </Typography>
         </div>
 
         <div className="buttons">
-        <Link to="/buddy">           
-          <Button variant="contained" color="primary" size="large">
-            Quero ser buddy
-          </Button>
-        </Link>
-        <Link to="/user">           
-          <Button variant="contained" color="primary" size="large">
-            Quero ser ouvido
-          </Button>
-        </Link>
+          <Link to="/buddy">
+            <Button variant="contained" color="primary" size="large">
+              Quero ser buddy
+            </Button>
+          </Link>
+          <Link to="/user">
+            <Button variant="contained" color="primary" size="large">
+              Quero ser ouvido
+            </Button>
+          </Link>
+          <Link to="/login">
+            <Button variant="outlined" color="secondary">
+              Login
+            </Button>
+          </Link>
         </div>
         <div className="buttons">
-        <Link to="/login">           
-          <Button variant="outlined" color="secondary">
-            Login
-          </Button>
-        </Link>
+
           </div>
       </Background>
     </div>
