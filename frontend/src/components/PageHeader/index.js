f69b798742422c5edc211e64879d462ade19e046
guilhermepdc <guilherme.carvalho@edu.pucrs.br>
2020-10-04 07:16:51 -0300

    alteracao na header

--- frontend/src/components/PageHeader/index.js
+++ frontend/src/components/PageHeader/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from '../../assets/img/logo.png';
+import Logo from '../../assets/img/logovec.png';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 import './styles.css';
