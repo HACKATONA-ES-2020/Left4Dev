76242d31316a44ce57d8164db3cf78c74fa0e6c1
Andre Marques <andremarques2911@gmail.com>
2020-10-04 01:51:58 -0300

    Criar tela de login

---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,88 @@
+import React from 'react';
+
+import Background from '../../components/Background';
+import ButtonDefault from '../../components/ButtonDefult';
+import Logo from '../../assets/img/logo.png';
+
+import { Link } from 'react-router-dom';
+
+import { TextField, IconButton, OutlinedInput, InputLabel, InputAdornment, FormControl} from '@material-ui/core';
+
+import { Visibility, VisibilityOff } from '@material-ui/icons';
+
+import './styles.css';
+
+function Login() {
+  const [values, setValues] = React.useState({
+    password: '',
+    showPassword: false,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const login = () => {
+    console.log("CHAMA API PRA LOGIN");
+  }
+
+  return (
+    <React.Fragment>
+      <div className="form-container">
+        <Background>
+          <div className="content-logo">
+            <img src={Logo} alt="Buddy" />
+          </div>
+          <div className="form-input">
+            <TextField
+              id="username"
+              label="Nome de Usuário / Apelido"
+              variant="outlined"
+              className="field"
+            />
+            <FormControl className="field" variant="outlined">
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <OutlinedInput
+                id="password"
+                type={values.showPassword ? 'text' : 'password'}
+                value={values.password}
+                onChange={handleChange('password')}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                labelWidth={70}
+              />
+            </FormControl>
+
+            <ButtonDefault value="Entrar" size="medium" onCLick={login} />
+            
+            <span className="text">Não possui login?
+              <Link to="/chose-tyoe">
+                <span>Deseja se cadastrar?</span>
+              </Link>
+            </span>
+          </div>
+        </Background>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Login;
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,12 +4,13 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import BuddyForm from './pages/BuddyForm';
 import UserForm from './pages/UserForm';
+import Login from './pages/Login';
 
 
 function Routes() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Landing}/>
+      <Route path="/" exact component={Login}/>
       <Route path="/buddy" component={BuddyForm}/>
       <Route path="/user" component={UserForm}/>
     </BrowserRouter>
